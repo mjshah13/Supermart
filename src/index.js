01be627b7ed8c5e68686937ee99f0173ab55e870
Muhammad Maaz <m.jshah13#gmail.com>
2019-04-20 10:56:32 -0700

    slideradd

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/Header';
 import Buttons from './components/Buttons';
-
+import Slider from './components/Slider';
 const App=()=>{
     return(
         <div>
             <Header/>
             <Buttons/>
+            <Slider/>
         
         </div>
     )
